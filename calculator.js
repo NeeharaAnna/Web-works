809cf09f6d97032c6470a71415a6936aaf5b3129
NeeharaAnna <neeharaannabince@gmail.com>
2025-02-23 11:17:33 +0530

    calculator adding

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function perform(val)
+{
+	console.log(val);
+
+	document.getElementById("display").value+=val;
+}
+function clearscreen()
+{
+	document.getElementById("display").value=""
+}
+function equalclick()
+{
+	var text=document.getElementById("display").value
+	var result=eval(text)
+	document.getElementById("display").value=result
+}
